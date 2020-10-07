5b951cef70975977f00eadba1611a8b838a3025b
David Egbert <dmegbert@gmail.com>
2020-10-06 21:19:14 -0400

    Update residencies link

--- blk-blur-ui/src/BlkBlur/LandingPage.js
+++ blk-blur-ui/src/BlkBlur/LandingPage.js
@@ -94,7 +94,7 @@ const LandingPage = () => {
         </Grid>
         <Grid item>
           <Link
-            href="https://www.ortvi.art/programs/blkblur-bumper"
+            href="https://www.ortvi.art/programs/mab_residencies_rr_1updatemp4-68fe93"
             underline="none"
             target="_blank"
           >
